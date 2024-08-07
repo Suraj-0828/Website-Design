19bc9077b9cb40c560bcbbcd5566f037db7c1809
Suraj Jha <134250774+Suraj-0828@users.noreply.github.com>
2024-08-06 19:36:29 -1000

    Add files via upload

---
+++ Step8 Success & Failed Pages/failed.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<head>
+	<!-- Title and Discription start -->
+	<meta charset="UTF-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Submission Failed</title>
+
+	<meta name="description" content="Oops! Something went wrong, and your form submission was unsuccessful. Please try again later or contact us directly at [insert contact details]. We apologize for the inconvenience.">
+
+	<meta name="Keywords" content="Here you type your SEO content..... Key like.......">
+
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="1 day">
+	<meta name="language" content="English">
+	<meta name="generator" content="N/A">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Pragma" content="no-cache" />
+	<!-- Title and Discription close -->
+
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="images/fevicon.png">
+	<!-- CSS
+         ============================================ -->
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<style>
+		.success {
+			text-align: center;
+			margin: auto;
+			background-color: gray;
+			color: black;
+		}
+
+		.successful {
+			text-align: center;
+			margin: auto;
+			padding: 40px 50px 20px 50px;
+			font-size: 50px !important;
+			color: black;
+		}
+
+		img {
+			max-width: 100%;
+		}
+
+		.img-size {
+			width: 100px;
+			height: 100px;
+			margin: auto;
+			margin-top: 25px;
+			margin-bottom: -20px;
+		}
+
+		p {
+			text-align: center;
+		}
+
+		@media (max-width: 576px) {
+			/* Extra small devices (phones) */
+			.successful {
+				font-size: 20px !important;
+			}
+		}
+	</style>
+</head>
+
+<body>
+	<!-- Page Wrapper -->
+	<div class="page-wrapper">
+
+		<!-- Header Start -->
+		<?php include 'header.php'; ?>
+		<!-- Header Close -->
+
+		<!-- Title Bar -->
+		<div class="pbmit-title-bar-wrapper">
+			<div class="container">
+				<div class="pbmit-title-bar-content">
+					<div class="pbmit-title-bar-content-inner">
+						<div class="pbmit-tbar">
+							<div class="pbmit-tbar-inner container">
+								<h1 class="pbmit-tbar-title">Failed</h1>
+							</div>
+						</div>
+						<div class="pbmit-breadcrumb">
+							<div class="pbmit-breadcrumb-inner">
+								<span>
+									<a title="" href="https://shilpkarfitness.com/" class="home"><span>Home</span></a>
+								</span>
+								<span class="sep">
+									<i class="pbmit-base-icon-angle-double-right"></i>
+								</span>
+								<span><span class="post-root post post-post current-item">Failed</span></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Title Bar End-->
+
+		<!-- Page Content -->
+		<div class="page-content">
+
+
+			<div class="container pt-5 pb-4">
+				<div class="img-size">
+					<img src="images/other/multiply.png" />
+				</div>
+				<div class="successful text-black">
+					Your Email Has Not Send!
+				</div>
+				<p class="text-black">Something went wrong please try again after sometime.</p>
+
+			</div>
+
+
+			<!-- Page Content End -->
+
+			<!-- Footer Start -->
+			<?php include 'footer.php'; ?>
+			<!-- Footer Close -->
---
+++ Step8 Success & Failed Pages/success.php
@@ -0,0 +1,134 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+
+<head>
+	<!-- Title and Discription start -->
+	<meta charset="UTF-8" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>Form Submitted Successfully</title>
+
+	<meta name="description" content="Thank you for reaching out to Shilpkar Fitness! Your form has been submitted successfully. Our team will get back to you shortly. Stay fit and healthy!">
+
+	<meta name="Keywords" content="Here you type your SEO content..... Key like.......">
+
+	<meta name="robots" content="index, follow">
+	<meta name="revisit-after" content="1 day">
+	<meta name="language" content="English">
+	<meta name="generator" content="N/A">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Pragma" content="no-cache" />
+	<!-- Title and Discription close -->
+
+
+	<!-- Favicon -->
+	<link rel="shortcut icon" type="image/x-icon" href="images/fevicon.png">
+	
+
+	<!-- Style CSS -->
+	<link rel="stylesheet" href="css/style.css">
+
+
+	<style>
+		.success {
+			text-align: center;
+			margin: auto;
+			background-color: gray;
+			color: black;
+		}
+
+		.successful {
+			text-align: center;
+			margin: auto;
+			padding: 40px 50px 20px 50px;
+			font-size: 45px !important;
+			color: black;
+		}
+
+		img {
+			max-width: 100%;
+		}
+
+		.img-size {
+			width: 100px;
+			height: 100px;
+			margin: auto;
+			margin-top: 25px;
+			margin-bottom: -20px;
+		}
+
+		p {
+			text-align: center;
+		}
+
+		@media (max-width: 576px) {
+
+			/* Extra small devices (phones) */
+			.successful{
+				font-size: 15px !important;
+			}
+		}
+	</style>
+</head>
+
+<body>
+	<!-- Page Wrapper -->
+	<div class="page-wrapper">
+
+		<!-- Header Start -->
+		<?php include 'header.php'; ?>
+		<!-- Header Close -->
+
+		<!-- Title Bar -->
+		<div class="pbmit-title-bar-wrapper">
+			<div class="container">
+				<div class="pbmit-title-bar-content">
+					<div class="pbmit-title-bar-content-inner">
+						<div class="pbmit-tbar">
+							<div class="pbmit-tbar-inner container">
+								<h1 class="pbmit-tbar-title">Success</h1>
+							</div>
+						</div>
+						<div class="pbmit-breadcrumb">
+							<div class="pbmit-breadcrumb-inner">
+								<span>
+									<a title="" href="https://shilpkarfitness.com/" class="home"><span>Home</span></a>
+								</span>
+								<span class="sep">
+									<i class="pbmit-base-icon-angle-double-right"></i>
+								</span>
+								<span><span class="post-root post post-post current-item">Success</span></span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Title Bar End-->
+
+		<!-- Page Content -->
+		<div class="page-content">
+
+
+
+			<div class="container pt-5 pb-4">
+				<div class="img-size">
+					<img src="images/other/success.png" />
+				</div>
+				<div class="successful text-black">
+					Your Request Has Been Successfully Submited
+				</div>
+				<p class="text-black">Our dedicated care coordinator will reach out to you as soon as possible to assist you further.</p>
+
+			</div>
+
+
+
+
+			<!-- Page Content End -->
+
+			<!-- Footer Start -->
+			<?php include 'footer.php'; ?>
+			<!-- Footer Close -->
